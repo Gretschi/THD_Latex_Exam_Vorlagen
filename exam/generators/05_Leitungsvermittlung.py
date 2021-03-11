fd9a00c609139934b373b21cc0e605e364865989
Andreas Fischer <andreas.fischer@th-deg.de>
2021-03-11 10:05:55 +0100

    Simplified circuit switching example

--- exam/generators/05_Leitungsvermittlung.py
+++ exam/generators/05_Leitungsvermittlung.py
@@ -1,23 +1,9 @@
 FILES = ["05_Rechnung_Leitungsvermittlung.tex"]
 
-def get_basic_ctx(rng):
+def get_context(rng):
     return {
         "datarate": rng.randint(10, 100),
         "channels": rng.randint(10, 30),
         "switchingdelay": rng.randint(50, 1000),
         "data": rng.randint(10, 900),
         }
-
-def compute_solution(ctx):
-    channelrate = ctx["datarate"]/ctx["channels"]
-    transdelay = ctx["data"]/channelrate
-    result = ctx["switchingdelay"] + transdelay
-    ctx["channelrate"] = round(channelrate, 2)
-    ctx["transdelay"] = round(transdelay, 2)
-    ctx["result"] = round(result, 2)
-    return ctx
-
-def get_context(rng):
-    ctx = get_basic_ctx(rng)
-    ctx = compute_solution(ctx)
-    return ctx
